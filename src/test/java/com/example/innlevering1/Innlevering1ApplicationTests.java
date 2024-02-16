44908cc9bde4ed8c4d794a9da3a01a7870b23ccc
Jørgen Kleppan <jokle5828@oslomet.no>
2024-02-16 18:18:00 +0100

    First commit

---
+++ src/test/java/com/example/innlevering1/Innlevering1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.innlevering1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Innlevering1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
